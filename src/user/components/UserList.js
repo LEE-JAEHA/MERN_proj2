a3780ba801f9065c1cb19fd0c776210164b7e58b
LEE-JAEHA <years6666@gmail.com>
2023-05-15 20:49:33 +0900

    JAEHA : Add Navigation + side

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -2,12 +2,16 @@ import React, { useSyncExternalStore } from 'react';
 
 import "./UserList.css"
 import UserItem from './UserItem';
+import Card from "../../shared/components/UIElements/Card";
+import "./UserList.css";
 const UserList = props  =>{
     // 사용자가 있고 없고 차이로
     // 여기서는 Props에 컴포넌트를 Item 으로 지정 
     if(props.items.length ===0 ){
         return <div className="center">
-            <h2>No Users</h2>
+            <Card>
+                <h2>No Users</h2>
+            </Card>
         </div>
     }
     return( 
