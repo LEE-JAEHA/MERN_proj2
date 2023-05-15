f11effd04cadc5da4df59c7b5e0b8f5673af435a
LEE-JAEHA <years6666@gmail.com>
2023-05-16 08:52:51 +0900

    JAEHA : Side bar developing ...

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import {BrowserRouter as Router, Route , Redirect,Switch} from 'react-router-dom
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 // ES
-
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 // 라우터를 사용하는 앱 일부 감싸기 
 // Route의 경우 Router가 감싸고 있을 때만 사용 가능
 const App =() => {
-  return <Router>
-    <Switch>
-      <Route path = "/users">
-        <Users/>
-      </Route>
-      <Route path = "/newplace">
-        <NewPlace/>
-      </Route>
-      <Redirect to = "/users" /> 
-      {/* /뒤에 뭐가 오더라도 같은 곳을 가게*/}
-    </Switch>
+  return (
+  <Router>
+    <MainNavigation/>
+    <main>
+      <Switch>
+        <Route path = "/users">
+          <Users/>
+        </Route>
+        <Route path = "/newplace">
+          <NewPlace/>
+        </Route>
+        <Redirect to = "/users" /> 
+        {/* /뒤에 뭐가 오더라도 같은 곳을 가게*/}
+      </Switch>
+    </main>
 
-  </Router>
-  
-  ;
+  </Router>);
 }
 
 export default App;
---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MainHeader  from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+
+import "./MainNavigation.css"
+const MainNavigation = props =>{
+    return( 
+        <React.Fragment>
+            <SideDrawer className="main-navigation__drawer-bar">
+                <nav>
+                    <NavLinks/>
+                    
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className="main-navigation__menu-btn">
+                    <span/>
+                    <span/>
+                    <span/>
+                </button>
+                <h1 className = "main-navigation__title"><Link to ="/">Your Places</Link></h1>
+                <nav>
+                    <NavLinks/>
+                </nav>
+            </MainHeader>
+        </React.Fragment>
+        );
+};
+export default MainNavigation;
